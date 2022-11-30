32b4b7e2bf49302f2067468614cee50cde33b6da
gmalka <anhae@bk.ru>
2022-11-30 16:02:59 +0300

    6

--- Listener/main.go
+++ Listener/main.go
@@ -12,16 +12,33 @@ func main() {
 		messageBroker = "nats://nats:8222"
 	)
 	log.Println("Connecting to nats")
-	nc, _ := nats.Connect(messageBroker)
+	nc, err := nats.Connect(messageBroker)
+	for err != nil {
+		nc, err = nats.Connect(messageBroker)
+		log.Println("connecting to nats...")
+		time.Sleep(time.Second)
+	}
 	defer nc.Close()
 	log.Println("Connected to nats")
 
-	js, _ := nc.JetStream()
+	js, err := nc.JetStream()
+	for err != nil {
+		js, err = nc.JetStream()
+		log.Println("connecting to jetstream...")
+		time.Sleep(time.Second)
+	}
 
 	log.Println("Waiting for messages...")
-	tr, _ := js.Subscribe("USERS.*", func(msg *nats.Msg) {
+	tr, err := js.Subscribe("USERS.*", func(msg *nats.Msg) {
 		fmt.Printf("%s address on %s\n", msg.Subject, msg.Data)
 	})
+	for err != nil {
+		tr, err = js.Subscribe("USERS.*", func(msg *nats.Msg) {
+		fmt.Printf("%s address on %s\n", msg.Subject, msg.Data)
+	})
+		log.Println("connecting to stream...")
+		time.Sleep(time.Second)
+	}
 	defer tr.Unsubscribe()
 	time.Sleep(10 * time.Minute)
 }
